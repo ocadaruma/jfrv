4806da23e0c2f4ddbc257d2712f2b5dfcae3e098
Haruki Okada <ocadaruma@gmail.com>
2022-07-20 23:51:05 +0900

    wip-canvas

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <template>
   <div>
-    <div class="h-6"></div>
-    <nav class="h-6 fixed top-0 left-0 z-50 bg-slate-200 w-full">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/jfr">JFR Viewer</router-link>
-    </nav>
-    <router-view/>
+    <div class="h-12 fixed top-0 left-0 z-50 w-full bg-white border-b border-slate-400">
+      <nav class="h-8 m-2 flex space-x-2">
+        <router-link to="/" class="hover:bg-slate-300 flex-none w-24 h-7 text-sm text-center border-2 rounded border-slate-400">
+          <span class="align-middle">home</span>
+        </router-link>
+        <router-link to="/jfr" class="hover:bg-slate-300 flex-none w-24 h-7 text-sm text-center border-2 rounded border-slate-400">
+          <span class="align-middle">jfr</span>
+        </router-link>
+      </nav>
+    </div>
+    <div class="fixed top-12 left-0 right-0 bottom-0">
+      <router-view />
+    </div>
   </div>
 </template>
--- src/views/JFRView.vue
+++ src/views/JFRView.vue
@@ -1,35 +1,30 @@
 <template>
-  <div>
-    <div v-for="thread in threads" :key="thread.id" class="h-4 text-sm">
-      <div class="flex w-fit">
-        <div class="flex-none
-                    sticky
-                    left-0
-                    w-96
-                    border-r border-r-slate-500
-                    border-t border-t-slate-300
-                    bg-slate-50">
-          <div class="truncate">{{ thread.name }}</div>
-        </div>
-        <div class="grow border-t border-t-slate-300">
-          <svg :width="chartArea().w">
-            <rect v-for="sample in perThreadSamples.get(thread.id)"
-                  :key="sample.timestamp"
-                  :x="chartArea().w * (sample.timestamp - profileDuration.startMillis) / (profileDuration.endMillis - profileDuration.startMillis)"
-                  y="4"
-                  :width="sampleRenderWidth"
-                  height="8"
-                  class="hover:fill-blue-300"
-                  :fill="threadStateColor(sample.threadStateId)"
-                  @click="clickSample(sample)"/>
-          </svg>
+  <div class="fixed top-12 left-0 w-12 bottom-0 bg-indigo-100 z-40 border-r border-slate-400 p-2">
+    <div class="flex flex-col space-y-2">
+      <div class="w-7 h-24 flex-none text-sm border-2 rounded border-slate-400 hover:bg-indigo-300 pt-2">
+        <div class="rotate-90">
+          <span>threads</span>
         </div>
       </div>
     </div>
-
-    <div class="w-full h-64" />
-    <div class="fixed bottom-0 w-full bg-slate-300 h-64">
-      <div v-for="frame in selectedFrames" :key="frame">{{ frame.typeName }}@{{ frame.methodName }}</div>
+  </div>
+  <div class="fixed top-12 left-12 h-8 right-0 bg-neutral-100 z-40 border-b border-slate-400 p-2">
+    <div class="flex flex-col space-x-2">
+    </div>
+  </div>
+  <div class="fixed h-32 left-12 right-0 bottom-0 bg-neutral-100 z-40 border-t-4 border-slate-400 border-double p-2 overflow-auto">
+    <div class="flex flex-col space-x-2">
+    </div>
+  </div>
+  <div class="fixed top-20 left-12 right-0 bottom-32 overflow-auto">
+    <div class="flex w-fit">
+      <div class="sticky text-sm left-0 bg-slate-100 border-r-4 border-double border-slate-300 min-h-full">
+        <svg ref="thread-chart-header" width="0" height="0"/>
+      </div>
+      <canvas ref="thread-chart-sample-view"
+              class="bg-slate-100"
+              width="0"
+              height="0"/>
     </div>
   </div>
 </template>
@@ -40,74 +35,171 @@ import { Frame, Profile, Sample, StackTrace } from '@/models/jfr/profile'
 import { readTextFile } from '@tauri-apps/api/fs'
 import { invoke } from '@tauri-apps/api'
 
-const CHART_WIDTH = 4096;
-const CHART_HEIGHT = 2048;
+interface Thread {
+  readonly id: number,
+  readonly name: string,
+}
+
+interface Dimension {
+  readonly width: number,
+  readonly height: number,
+}
+
+class DateInterval {
+  readonly startMillis: number
+  readonly endMillis: number
+  readonly durationMillis: number
+
+  constructor(startMillis: number, endMillis: number) {
+    this.startMillis = startMillis
+    this.endMillis = endMillis
+    this.durationMillis = endMillis - startMillis
+  }
+}
+
+interface ThreadChartConfig {
+  readonly headerWidth: number,
+  readonly fontSize: number,
+  readonly borderWidth: number,
+  readonly borderColor: string,
+  readonly margin: number,
+  readonly sampleRenderSize: Dimension,
+}
+
+interface ThreadProfile {
+  readonly interval: DateInterval,
+  // per-thread-id samples
+  readonly samples: { [id: number]: Sample[] }
+  // max sample num per thread
+  readonly maxSampleNum: number
+  // thread list sorted by name
+  readonly threads: Thread[]
+}
+
+const CHART_CONFIG: ThreadChartConfig = {
+  headerWidth: 360,
+  fontSize: 14, // 0.875rem
+  borderWidth: 1,
+  borderColor: "#707070",
+  margin: 1,
+  sampleRenderSize: {
+    width: 6,
+    height: 8,
+  }
+}
 
 export default class JFRView extends Vue {
-  threads: {name: string, id: number}[] = []
-  stackTracePool: {[id: number]: StackTrace} = {}
-  threadStatePool: {[id: number]: string} = {}
-  perThreadSamples: Map<number, Sample[]> = new Map<number, Sample[]>()
-  profileDuration: {startMillis: number, endMillis: number} = {startMillis: 0, endMillis: 0}
-
+  stackTracePool: { [id: number]: StackTrace } = {}
+  threadStatePool: { [id: number]: string } = {}
   selectedFrames: Frame[] = []
-  sampleRenderWidth = 0
 
   async mounted() {
+    console.log("start")
     const filePath: string = await invoke("jfr_file_path")
     const text = await readTextFile(filePath)
     const profile: Profile = JSON.parse(text)
-    const perThreadSamples = new Map<number, Sample[]>()
+    console.log("parsed")
+
     this.stackTracePool = profile.stackTracePool
     this.threadStatePool = profile.threadStatePool
 
+    const threadProfile = this.convertProfile(profile)
+    console.log("converted")
+
+    const header = this.$refs["thread-chart-header"] as HTMLElement
+    const sampleView = this.$refs["thread-chart-sample-view"] as HTMLCanvasElement
+
+    const rowHeight = CHART_CONFIG.fontSize + CHART_CONFIG.margin * 2
+    const height = rowHeight * threadProfile.threads.length
+    const sampleViewWidth = CHART_CONFIG.sampleRenderSize.width * threadProfile.maxSampleNum
+
+    // adjust sizes
+    header.setAttribute("width", String(CHART_CONFIG.headerWidth))
+    header.setAttribute("height", String(height))
+    sampleView.width = sampleViewWidth
+    sampleView.height = height
+
+    const ctx = sampleView.getContext("2d", { alpha: false })!
+    for (let i = 0; i < threadProfile.threads.length; i++) {
+      const thread = threadProfile.threads[i]
+      const yOffset = rowHeight * i;
+
+      const text = document.createElementNS("http://www.w3.org/2000/svg", "text")
+      const textNode = document.createTextNode(thread.name)
+      text.setAttribute("x", String(CHART_CONFIG.margin))
+      // y is the baseline of the text.
+      // so we add fontSize to the current offset.
+      // also add margin to allocate the margin-top.
+      text.setAttribute("y", String(yOffset + CHART_CONFIG.fontSize + CHART_CONFIG.margin))
+      text.appendChild(textNode)
+      header.appendChild(text)
+
+      if (i < threadProfile.threads.length - 1) {
+        const y = yOffset + rowHeight
+        const line = document.createElementNS("http://www.w3.org/2000/svg", "line")
+        line.setAttribute("x1", String(0))
+        line.setAttribute("y1", String(y))
+        line.setAttribute("x2", String(CHART_CONFIG.headerWidth))
+        line.setAttribute("y2", String(y))
+        line.setAttribute("stroke-width", String(CHART_CONFIG.borderWidth))
+        line.setAttribute("stroke", String(CHART_CONFIG.borderColor))
+        header.appendChild(line)
+      }
+
+      const samples = threadProfile.samples[thread.id]
+      for (let j = 0; j < samples.length; j++) {
+        const sample = samples[j]
+        const x = sampleViewWidth * (sample.timestamp - threadProfile.interval.startMillis) /
+            threadProfile.interval.durationMillis
+        const y = yOffset + (rowHeight - CHART_CONFIG.sampleRenderSize.height) / 2
+
+        const stateName = this.threadStatePool[sample.threadStateId]
+        let fillColor = "#6f6d72"
+        if (stateName === "STATE_RUNNABLE") {
+          fillColor = "#6cba1e";
+        }
+        if (stateName === "STATE_SLEEPING") {
+          fillColor = "#8d3eee";
+        }
+
+        ctx.fillStyle = fillColor
+        ctx.fillRect(x, y, CHART_CONFIG.sampleRenderSize.width, CHART_CONFIG.sampleRenderSize.height)
+      }
+    }
+
+    console.log("rendered")
+  }
+
+  private convertProfile(profile: Profile): ThreadProfile {
+    const perThreadSamples: { [id: number]: Sample[] } = {}
+
     let startMillis = Infinity
     let endMillis = -1
+    let maxSamples = 0
 
+    const threads: Thread[] = []
     profile.samples.forEach(sample => {
       startMillis = Math.min(startMillis, sample.timestamp)
       endMillis = Math.max(endMillis, sample.timestamp)
 
-      if (perThreadSamples.has(sample.threadId)) {
-        const samples = perThreadSamples.get(sample.threadId)!
-        samples.push(sample)
-      } else {
-        perThreadSamples.set(sample.threadId, [sample])
+      let samples = perThreadSamples[sample.threadId]
+      if (samples === undefined) {
+        threads.push({ id: sample.threadId, name: profile.threadNamePool[sample.threadId] })
+        samples = []
       }
+      samples.push(sample)
+      perThreadSamples[sample.threadId] = samples
+      maxSamples = Math.max(maxSamples, samples.length)
     })
 
-    this.perThreadSamples = perThreadSamples
-    this.profileDuration = {startMillis, endMillis}
+    threads.sort((a, b) => a.name.localeCompare(b.name))
 
-    const threads: {name: string, id: number}[] = []
-    let maxSamples = 0
-    perThreadSamples.forEach((v, k) => {
-      threads.push({name: profile.threadNamePool[k], id: k})
-      maxSamples = Math.max(maxSamples, v.length)
-    })
-
-    this.threads = threads.sort((a, b) => a.name.localeCompare(b.name))
-
-    this.sampleRenderWidth = CHART_WIDTH / maxSamples
-  }
-
-  threadStateColor(state: number): string {
-    const stateName = this.threadStatePool[state]
-    if (stateName === "STATE_RUNNABLE") {
-      return "#6cba1e";
+    return {
+      interval: new DateInterval(startMillis, endMillis),
+      samples: perThreadSamples,
+      maxSampleNum: maxSamples,
+      threads: threads
     }
-    if (stateName === "STATE_SLEEPING") {
-      return "#8d3eee";
-    }
-    return "#6f6d72";
-  }
-
-  chartArea(): {w: number, h: number} {
-    return {w: CHART_WIDTH, h: CHART_HEIGHT}
-  }
-
-  clickSample(sample: Sample) {
-    this.selectedFrames = this.stackTracePool[sample.stackTraceId].frames
   }
 }
 </script>
